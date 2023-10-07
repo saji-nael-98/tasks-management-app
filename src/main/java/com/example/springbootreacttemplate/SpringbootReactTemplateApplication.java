b1db0f7a5433e8ed195017ad35b4026ac959add0
saji-nael-98 <saji.nael.98@gmail.com>
2023-10-07 18:47:09 +0300

    init git

---
+++ src/main/java/com/example/springbootreacttemplate/SpringbootReactTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreacttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootreacttemplate/SpringbootReactTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreacttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
